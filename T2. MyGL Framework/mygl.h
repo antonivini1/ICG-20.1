23902b1abeeaab2ab20194b7d382428cc6de7261
Antoni Vinicius <72105624+antonivini1@users.noreply.github.com>
2020-10-20 17:43:28 -0300

    Update mygl.h

--- T2. MyGL Framework/mygl.h
+++ T2. MyGL Framework/mygl.h
@@ -18,17 +18,6 @@ struct ponto{
     int y;
 };
 typedef struct ponto Ponto;
-struct linha{
-    Ponto inicio;
-    Ponto fim;
-};
-typedef struct linha Linha;
-struct triangulo{
-    Ponto v0;
-    Ponto v1;
-    Ponto v2;
-};
-typedef struct triangulo Triangulo;
 
 // Declaração da função que chamará as funções implementadas pelo aluno
 void MyGlDraw(void);
@@ -37,13 +26,13 @@ void MyGlDraw(void);
 // >>> Declare aqui as funções que você implementar <<<
 //
 
-void PutPixel(Ponto coordenada, Cor rgba);
-void DrawLine(Ponto inicio, Ponto fim, Cor rgba1, Cor rgba2);
-void DrawTriangle(Ponto v0, Ponto v1, Ponto v2, Cor rgba0,Cor rgba1,Cor rgba2);
-Cor RetornaCor();
-Ponto CriaPonto();
-int modulo(int x);
-Ponto inverte(Ponto pt);
-Cor InterpolarCor(Cor a, Cor b, int deltaX);
+void PutPixel(Ponto coordenada, Cor RGBA);                                      //Desenha um pixel na tela
+void DrawLine(Ponto inicio, Ponto fim, Cor RGBAi, Cor RGBAf);                   //Desenha uma linha na tela
+void DrawTriangle(Ponto v0, Ponto v1, Ponto v2, Cor RGBA0,Cor RGBA1,Cor RGBA2); //Desenha um triangulo na tela
+// -----------------------------------------------------------------------------
+int modulo(int x);                                                              //Alternativa para abs()
+Ponto inverte(Ponto pt, int i);                                                 //Inverte x e y, podendo x = +-y
+Ponto reverte(Ponto pt,int r);                                                  //Reverte x e y
+Cor InterpolarCor(Cor RGBA1, Cor RGBA2, int dx);                                //Interpola as cores dos pontos
 
 #endif  // MYGL_H
